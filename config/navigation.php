b819f0fde680edc7c0eb117f999de277318d83f8
rastkotodorovic <rale.todorovic2@gmail.com>
2021-09-29 14:06:57 +0200

    Navigation config structure

--- config/navigation.php
+++ config/navigation.php
@@ -8,7 +8,18 @@ return [
     // 'Pages' => [
     //     'icon' => '<svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" class="mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 21h10a2 2 0 002-2V9.414a1 1 0 00-.293-.707l-5.414-5.414A1 1 0 0012.586 3H7a2 2 0 00-2 2v14a2 2 0 002 2z" /></svg>',
     //     'pages' => [
-    //
+    //         'Tests' => [
+    //             [
+    //                 'name' => 'Show all tests',
+    //                 'route' => 'admin.tests.index',
+    //             ],
+    //             [
+    //                 'name' => 'Create new tests',
+    //                 'route' => 'admin.tests.create',
+    //                 'can' => 'tests create'
+    //             ],
+    //             'can' => 'tests viewAny'
+    //         ],
     //     ],
     // ],
     // 'Settings' => [
