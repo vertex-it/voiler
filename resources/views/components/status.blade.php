2e08f6b4e0fd13397c808fda69933d7be76f2db9
rastkotodorovic <rale.todorovic2@gmail.com>
2021-10-08 10:59:08 +0200

    Status color fix

--- resources/views/components/status.blade.php
+++ resources/views/components/status.blade.php
@@ -5,7 +5,7 @@
     ></div>
 @else
     <div
-        class="status-pill {{ $status ? 'green' : 'red' }}"
+        class="status-pill {{ $status ? 'bg-green-400' : 'bg-red-400' }}"
         title="{{ $status ? __('Active') : __('Inactive') }}"
     ></div>
 @endif
