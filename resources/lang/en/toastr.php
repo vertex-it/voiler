21a338fc0d88781deb64b1c7cb967d6ce283f556
rastkotodorovic <rale.todorovic2@gmail.com>
2021-10-11 15:09:36 +0200

    Toastr translations

---
+++ resources/lang/en/toastr.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'clone' => 'U have successfully cloned',
+    'store' => 'U have successfully created',
+    'update' => 'U have successfully updated',
+    'destroy' => 'U have successfully deleted',
+    'default' => 'U have successfully done this action',
+];
---
+++ resources/lang/sr/toastr.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'clone' => 'Uspješno ste klonirali',
+    'store' => 'Uspješno ste dodali',
+    'update' => 'Uspješno ste izmijenili',
+    'destroy' => 'Uspješno ste obrisali',
+    'default' => 'Uspješno ste izvršili akciju',
+];
