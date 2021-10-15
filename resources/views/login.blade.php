85903039c3c5521d8444ad6b238cc30ac58d2cb3
Mile Panic <mile.panic96@gmail.com>
2021-10-15 16:41:50 +0200

    Feature/UI improvements (#8)
    * Datatables ui improvements
    
    * Datatables action button
    
    * Datatable inputs, status icons
    
    * Login page

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -2,89 +2,49 @@
 <html>
 <head>
     <title>Login | {{ config('app.name') }}</title>
-    <meta charset="utf-8">
-    <meta content="width=device-width, initial-scale=1" name="viewport">
-    <meta content="ie=edge" http-equiv="x-ua-compatible">
     <link href="{{ asset('favicon.ico') }}">
-    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('apple-touch-icon.png') }}">
-    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon-32x32.png') }}">
-    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon-16x16.png') }}">
-    <link rel="manifest" href="{{ asset('site.webmanifest') }}">
-    <link rel="mask-icon" href="{{ asset('safari-pinned-tab.svg') }}" color="#5bbad5">
-    <meta name="msapplication-TileColor" content="#ffffff">
-    <meta name="theme-color" content="#ffffff">
-    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@500;600;700&display=swap" rel="stylesheet" type="text/css">
-    <link href="{{ asset('assets/admin2/css/main.css') }}" rel="stylesheet">
+
+    <meta charset="UTF-8">
+    <meta content="width=device-width, initial-scale=1" name="viewport">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,500;0,700;0,900;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    <style>
+        * {
+            font-family: 'Roboto', sans-serif;
+        }
+
+        body {
+            background-color: #f9fafb;
+            background-image: url("data:image/svg+xml,%3Csvg width='12' height='24' viewBox='0 0 12 24' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='none' fill-rule='evenodd'%3E%3Cg fill='%23eaeaea' fill-opacity='0.4'%3E%3Cpath d='M2 0h2v12H2V0zm1 20c1.105 0 2-.895 2-2s-.895-2-2-2-2 .895-2 2 .895 2 2 2zM9 8c1.105 0 2-.895 2-2s-.895-2-2-2-2 .895-2 2 .895 2 2 2zm-1 4h2v12H8V12z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E");
+        }
+    </style>
+
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
-<body class="auth-wrapper">
-    <div class="all-wrapper menu-side with-pattern" style="padding: 10px!important;">
-        <div class="auth-box-w">
-            <div class="logo-w" style="padding: 60px;">
-                <img alt="logo" src="{{ asset('logo.png') }}" width="180">
+<body>
+    <div class="container mx-auto h-screen flex items-center" style="max-width: 560px;">
+        <div class="section m-0 w-full">
+            <div class="section-content">
+                <div class="card">
+                    <div class="py-8">
+                        <p class="text-center text-2xl tracking-wide text-blue-500 font-semibold mb-3">{{ config('app.name') }}</p>
+                        <h1 class="font-bold text-gray-800 text-3xl text-center">{{ __('Sign into your account') }}</h1>
+                    </div>
+                    <hr class="my-10 border-gray-100">
+                    <x-form
+                        action="{{ route('login') }}"
+                        method="POST"
+                        buttonText="{{ __('Log in') }}"
+                        buttonClasses="w-full mt-10"
+                    >
+                        <x-inputs.input name="email" type="email" fullWidth />
+                        <x-inputs.input name="password" type="password" fullWidth />
+                        <x-inputs.toggle name="remember" label="{{ __('Remember me') }}" value="true" />
+                    </x-form>
+                </div>
             </div>
-            <h4 class="auth-header" style="padding: 0 20px 20px 20px!important;" >
-                Ulogujte se
-            </h4>
-            <form
-                action="{{ route('login') }}"
-                method="POST"
-                style="padding: 20px 20px!important;"
-            >
-                {{ csrf_field() }}
-                <div class="form-group {{ $errors->has('email') ? 'has-error' : '' }}">
-                    <label for="email">Email</label>
-                    <input
-                        class="form-control"
-                        placeholder="Unesite email"
-                        type="text"
-                        style="font-size: 16px!important;"
-                        id="email"
-                        name="email"
-                        value="{{ old('email') }}"
-                        required
-                        autofocus
-                    >
-                    @if ($errors->has('email'))
-                        <span class="help-block">
-                                    <strong>{{ $errors->first('email') }}</strong>
-                                </span>
-                    @endif
-                </div>
-                <div class="form-group {{ $errors->has('password') ? 'has-error' : '' }}">
-                    <label for="password">Lozinka</label>
-                    <input
-                        class="form-control"
-                        placeholder="Unesite lozinku"
-                        style="font-size: 16px!important;"
-                        type="password"
-                        id="password"
-                        name="password"
-                        required
-                    >
-                    @if ($errors->has('password'))
-                        <span class="help-block">
-                                    <strong>{{ $errors->first('password') }}</strong>
-                                </span>
-                    @endif
-                </div>
-                <div class="form-group">
-                    <label
-                        class="form-check-label"
-                        style="margin: 0 20px!important;"
-                    >
-                        <input
-                            class="form-check-input"
-                            type="checkbox"
-                            name="remember"
-                            {{ old('remember') ? 'checked' : '' }}
-                        >
-                        Zapamti me
-                    </label>
-                </div>
-                <div class="buttons-w">
-                    <button class="btn btn-primary">Login</button>
-                </div>
-            </form>
         </div>
     </div>
 </body>
