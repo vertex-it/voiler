07011f3a6017c604df5e57f67b3304f5be551d5e
Mile Panic <mile.panic96@gmail.com>
2022-06-20 16:39:12 +0200

    Replace avatar image with svg

--- resources/views/layouts/navigation/mobile-profile.blade.php
+++ resources/views/layouts/navigation/mobile-profile.blade.php
@@ -1,6 +1,8 @@
 <div class="flex items-center">
     <div class="flex-shrink-0">
-        <img class="h-10 w-10 rounded-full" src="http://placeimg.com/100/100/animals" alt="">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10" viewBox="0 0 20 20" fill="currentColor">
+            <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clip-rule="evenodd" />
+        </svg>
     </div>
     <div class="ml-3">
         <div class="text-sm font-medium text-gray-800">{{ Auth::user()->getTitle() }}</div>
--- resources/views/layouts/sidebar/profile.blade.php
+++ resources/views/layouts/sidebar/profile.blade.php
@@ -1,7 +1,9 @@
 <div class="ml-3 dropdown direction-down-left">
     <a href="#" aria-current="page" aria-expanded="false" aria-haspopup="true">
         <span class="sr-only">Open user menu</span>
-        <img class="h-8 w-8 rounded-full" src="http://placeimg.com/100/100/animals" alt="">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" viewBox="0 0 20 20" fill="currentColor">
+            <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clip-rule="evenodd" />
+        </svg>
     </a>
 
     <div
