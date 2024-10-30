eaa3f6566d8dd9ffda97b0c64f7c025279f4d18f
Mile Panic <mile.panic96@gmail.com>
2024-10-30 16:25:33 +0100

    Bugfix unavailable primary color

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,3 +7,7 @@ All notable changes to `voiler` will be documented in this file
 - Start following Laravel's versioning
 - Upgrade datatables to 3.1, improve styling, add checkbox for select, many more small tweaks
 - Define primary color as 4 shades of specific HEX colors
+
+## v11.1.1 - 2024-10-30
+
+- Minor bugfix
--- resources/views/fortify/register.blade.php
+++ resources/views/fortify/register.blade.php
@@ -3,15 +3,15 @@
     <head>
         <title>{{ __('voiler::interface.login') }} | {{ config('app.name') }}</title>
         <link href="{{ asset('favicon.ico') }}">
-        
+
         <meta charset="UTF-8">
         <meta content="width=device-width, initial-scale=1" name="viewport">
-        
+
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
-        
+
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     </head>
-    <body class="h-screen bg-gradient-to-tr from-blue-100 via-primary-100 to-blue-100">
+    <body class="h-screen bg-primary-50">
         <div class="container mx-auto h-full flex items-start md:items-center">
             <div class="w-full sm:max-w-lg p-5 md:p-8 mx-2 sm:mx-auto mt-16 md:mt-0 bg-white rounded-lg shadow-lg">
                 <img
@@ -26,7 +26,7 @@
                 <h2 class="mt-10 md:mt-12 lg:mt-14 text-xl md:text-2xl text-gray-700">
                     {{ __('voiler::interface.sign_in_to_your_account') }}
                 </h2>
-                
+
                 <div class="mt-8">
                     <x-form
                         action="{{ route('register') }}"
@@ -37,7 +37,7 @@
                         <x-inputs.input name="email" type="email" />
                         <x-inputs.input name="password" type="password" />
                         <x-inputs.input name="password_confirmation" type="password" />
-                        
+
                         <div class="mt-14 mb-6">
                             {{--                        @if (Route::has('admin.register'))--}}
                             <p class="mt-3 text-xs md:text-sm text-center text-gray-500">
